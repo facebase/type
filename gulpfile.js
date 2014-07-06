f9f8460525b46530fbdcd92f1fa9bbe261927b3f
Michael Chan <mijoch@gmail.com>
2014-07-06 02:26:38 -0700

    WIP

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['sass']);
